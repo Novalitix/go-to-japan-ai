586f177c5af9621f88bcc415450e5903c7f57cc7
brellsanwouo <sanwouobrell@gmail.com>
2025-08-19 19:34:59 +0200

    Add training, replay, and testing functions for crew execution

--- src/go_to_japan/main.py
+++ src/go_to_japan/main.py
@@ -43,10 +43,81 @@ def run():
     """
     
     try:
-        print("Début de la mission :", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        resultats = GoToJapan().crew().kickoff(inputs=inputs)
-        print("Résultats de la mission :", resultats)
-        print("Fin de la mission :", datetime.now().strftime("%Y-%m-%d %H:%M:%S")) 
+        GoToJapan().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        'planningType': 'explore',
+        'travelWith': 'solo',
+        'pace': 'equilibre',
+        'firstName': 'Yann',
+        'departureDate': '2025-10-11',
+        'returnDate': '2025-10-13',
+        'duration': '2',
+        'departurePeriod': [],
+        'citiesToInclude': ['Kyoto'],
+        'citiesToExclude': [],
+        'budget': '5000',
+        'comments': '',
+        'interests': [
+            'temples'
+        ],
+        'services': [
+            'restaurants',
+            'lodging',
+        ],
+    }
+    try:
+        GoToJapan().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        GoToJapan().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        'planningType': 'explore',
+        'travelWith': 'solo',
+        'pace': 'equilibre',
+        'firstName': 'Yann',
+        'departureDate': '2025-10-11',
+        'returnDate': '2025-10-13',
+        'duration': '2',
+        'departurePeriod': [],
+        'citiesToInclude': ['Kyoto'],
+        'citiesToExclude': [],
+        'budget': '5000',
+        'comments': '',
+        'interests': [
+            'temples'
+        ],
+        'services': [
+            'restaurants',
+            'lodging',
+        ],
+    }
+
+    
+    try:
+        GoToJapan().crew().test(n_iterations=1, eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
